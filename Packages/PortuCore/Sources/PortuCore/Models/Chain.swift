468797398aea9f00e7ca4f26a4b77cb735615b5a
funny-otter <255084002+funny-otter@users.noreply.github.com>
2026-03-22 14:37:34 +0100

    feat: expand Chain enum with L2 and alt-chain support
    Add polygon, arbitrum, optimism, base, bsc, avalanche, monad, katana
    to support multi-chain position tracking.

--- Packages/PortuCore/Sources/PortuCore/Models/Chain.swift
+++ Packages/PortuCore/Sources/PortuCore/Models/Chain.swift
@@ -1,7 +1,13 @@
-import Foundation
-
 public enum Chain: String, Codable, CaseIterable, Sendable {
     case ethereum
+    case polygon
+    case arbitrum
+    case optimism
+    case base
+    case bsc
     case solana
     case bitcoin
+    case avalanche
+    case monad
+    case katana
 }
